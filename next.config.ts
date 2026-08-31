91c4dffbee8ae702f4fd86ab73b5270247913915
Hafiz Ahmad Hassan <ahmadhassan061@gmail.com>
2026-08-31 15:38:44 +0200

    Fix basePath + add images unoptimized for static export

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/context-engineering-lab4-tools-function-calls-mcp",
+  assetPrefix: "/context-engineering-lab4-tools-function-calls-mcp",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
